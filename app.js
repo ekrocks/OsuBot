c40e6e9d6a06d87db47799505186d79d19930fb1
pan-durr <michael.flores12349@gmail.com>
2023-03-23 21:13:00 -0600

    added app.js requiring password

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 const Banchojs = require("bancho.js");
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
+console.log(process.env.banchoUSERNAME)
